bc380db5c40aef6f542eb20bc3146e1b06ccda70
Teo Martin Toledo <teootoledo@gmail.com>
2021-07-12 15:21:59 -0300

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Navbar from "./components/UI-Kit/Navbar/index";
 import { Mainpage } from "./pages/Mainpage";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Navbar />
+      <Router>
+        <Navbar />
+      </Router>
       <Mainpage />
     </>
   );
